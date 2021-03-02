db724cbae859f63db09efef7a7c666420bf3496d
Константин Кирдянов <kirdyanovka@it2g.ru>
2021-03-02 17:00:21 +0300

    Добваление репозитория

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        readonly Book[] books = new[]
+        {
+            new Book (1, "Art Of programming"),
+            new Book (2, "Refactoring"),
+            new Book (3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(b => b.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
