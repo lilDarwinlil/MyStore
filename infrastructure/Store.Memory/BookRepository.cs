a957fcdda9f842d1befeacb11e7a4b3b6e1630dc
Константин Кирдянов <kirdyanovka@it2g.ru>
2021-03-04 15:18:16 +0300

    Рефакторинг

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,9 +7,9 @@ namespace Store.Memory
     {
         readonly Book[] books = new[]
         {
-            new Book (1, "Art Of Programming", "ISBN 12345-12345", "D. Knuth"),
-            new Book (2, "Refactoring", "ISBN 12345-12346", "M. Fowler"),
-            new Book (3, "C Programming Language", "ISBN 12345-12347", "B. Kernighan, D. Ritchie"),
+            new Book (1, "Art Of Programming", "ISBN 12345-12345", "D. Knuth", "Descrip1", 7.19m),
+            new Book (2, "Refactoring", "ISBN 12345-12346", "M. Fowler", "Descrip2", 12.45m),
+            new Book (3, "C Programming Language", "ISBN 12345-12347", "B. Kernighan, D. Ritchie", "Descrip3", 14.98m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -22,6 +22,11 @@ namespace Store.Memory
             return books.Where(b => b.Title.Contains(titleorAuthor)
                                  || b.Author.Contains(titleorAuthor))
                         .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(b => b.Id == id);
         }
     }
 }
