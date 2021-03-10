3da1d91a95146c6c9467ae506dc44593ae8fe8ce
Константин Кирдянов <kirdyanovka@it2g.ru>
2021-03-10 15:44:08 +0300

    +dto

---
+++ domain/Store/Data/BookDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Data
+{
+    public class BookDto
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public string Isbn { get; }
+
+        public string Author { get; }
+
+        public string Description { get; }
+
+        public decimal Price { get; }
+    }
+}
